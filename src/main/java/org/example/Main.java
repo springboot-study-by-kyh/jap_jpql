985a377e1f618342bd84f4808a8947ae7ddaccb4
sjh9391985 <sjh9391985@gmail.com>
2023-01-25 10:49:27 +0900

    query 타입 설정

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 public class Main {
 
@@ -17,6 +18,13 @@ public class Main {
         entityTransaction.begin();
 
         try {
+
+            Member  member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            entityManager.persist(member);
+
+            TypedQuery<Member> query = entityManager.createQuery("select m from Member m", Member.class);
 
             entityTransaction.commit();
         } catch (Exception e) {
